17b352fc3d161c4d2d170fc8d91440b72691f0ac
t7jari00 <t7jari00@students.oamk.fi>
2019-11-04 20:34:40 +0200

    moving, camera movement, shooting

---
+++ X-Mas_Boi$/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private int moveSpeed = 5;
+    private int rotateSpeed = 40;
+
+    void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        Vector3 MovementDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        MovementDirection = Camera.main.transform.TransformDirection(MovementDirection);
+        MovementDirection.y = 0.0f;
+        rb.transform.position += MovementDirection * moveSpeed * Time.deltaTime;
+
+        Vector3 PlayerRotation = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        rb.transform.Rotate(PlayerRotation * Time.deltaTime * rotateSpeed);
+
+        Vector3 CameraRotation = new Vector3(-Input.GetAxis("Mouse Y"), 0, 0);
+        Camera.main.transform.Rotate(CameraRotation * Time.deltaTime * rotateSpeed);
+    }
+}
---
+++ X-Mas_Boi$/Assets/Scripts/Sooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sooting : MonoBehaviour
+{
+    public Camera cm;
+    public int laserSpeed;
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Transform player = GetComponent<Transform>();
+            GameObject bullet = (GameObject)Instantiate(Resources.Load("Bullet"));
+            Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+            bulletRb.transform.position = player.position;
+
+            //Vector3 ShootingDirection = new Vector3(cm.transform.rotation.x, cm.transform.rotation.y, cm.transform.rotation.z);
+            //bulletRb.AddForce(ShootingDirection);
+
+            float x = Screen.width / 2f;
+            float y = Screen.height / 2f;
+
+            var ray = Camera.main.ScreenPointToRay(new Vector3(x, y, 0));
+            bulletRb.velocity = ray.direction * laserSpeed;
+        }
+    }
+}
