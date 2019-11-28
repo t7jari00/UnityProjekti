2894bb367d80490a17371a2b772ea0d33b0684e0
t7tyja00 <43406369+t7tyja00@users.noreply.github.com>
2019-11-28 20:16:36 +0200

    musiikki + voluumisettingsit

---
+++ X-Mas_Boi$/Assets/Scripts/Audiovolume.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Audiovolume : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    AudioSource menuaudio;
+    float slidervolume = 1f;
+    float volumepercentage;
+    public Slider slider;
+    public Text volumetext;
+    void Start()
+    {
+        GameObject steveGameObject = GameObject.Find("menutheme");
+        if (steveGameObject != null)
+        {
+            menuaudio = steveGameObject.GetComponent<AudioSource>();
+        }
+        slider.value = slidervolume;
+        volumetext.text = "Master volume: 100%";
+    }
+
+    void OnGUI()
+    {
+        menuaudio.volume = slider.value;
+        volumepercentage = slider.value * 100;
+        volumetext.text = "Master volume: " + System.Math.Round(volumepercentage, 0) + "%";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
